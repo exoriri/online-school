611b2daff68dc2ab3f82c236e00859006db08118
Lavrik Roman <lavrikrom2@gmail.com>
2022-01-23 03:45:51 +0300

    feat: preconnect

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -103,6 +103,8 @@ const HomePage = () => {
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
         <meta name="google" content="notranslate" key="notranslate" />
         <meta name="robots" content="all" />
+        <link rel="preconnect" href="https://www.google.com" />
+        <link rel="preconnect" href="https://www.gstatic.com" />
         <link rel="preload" href={'/main.webp'} as="image" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
