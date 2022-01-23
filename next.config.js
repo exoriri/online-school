f6e2a0464f87416cc8b9ca32a85e989b392cc3c6
Lavrik Roman <lavrikrom2@gmail.com>
2022-01-23 16:21:52 +0300

    feat: build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 module.exports = withBundleAnalyzer({
+  istDir: 'build',
   experimental: {
     // ssr and displayName are configured by default
     styledComponents: true,
